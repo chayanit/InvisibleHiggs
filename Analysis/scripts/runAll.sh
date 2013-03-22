7aeaf08486d38185c67c03538901a46ab01eebae
jbrooke <>
2013-03-22 14:21:06 +0000

    add datasets for wBG

--- Analysis/scripts/runAll.sh
+++ Analysis/scripts/runAll.sh
@@ -3,6 +3,7 @@
 IDIR=/storage/phjjb/invisibleHiggs/InvHiggsInfo_v9
 ODIR=InvHiggsInfo_v9
 DATASETS=InvisibleHiggs/Analysis/data/datasets_v9.txt
+DATASETS_W=InvisibleHiggs/Analysis/data/datasets_W_v9.txt
 LUMI=19560
 
 echo "controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
@@ -13,6 +14,9 @@ efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI
 
 echo "nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
 nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI
+
+echo "wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI"
+wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI
 
 echo "tar -zcvf $ODIR.tgz $ODIR/"
 tar -zcvf $ODIR.tgz $ODIR/
