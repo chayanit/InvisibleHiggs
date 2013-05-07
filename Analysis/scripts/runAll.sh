e2cd76ef8056bd1033950cbed8a466a06acf32c2
jbrooke <>
2013-05-07 16:37:35 +0000

    reinstate nminus one

--- Analysis/scripts/runAll.sh
+++ Analysis/scripts/runAll.sh
@@ -6,6 +6,8 @@ LUMI=19576
 IDIR=/storage/phjjb/invisibleHiggs/InvHiggsInfo_$VER
 ODIR=InvHiggsInfo_$VER
 
+mkdir $ODIR
+
 DATASETS=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_$VER.txt
 DATASETS_W=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_W_$VER.txt
 DATASETS_Z=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_Z_$VER.txt
@@ -13,35 +15,35 @@ DATASETS_QCD=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_QCD_$VER.txt
 
 # MC numbers & plots
 echo "efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
-efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI
+efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/efficiency.log
 
 echo "nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
 echo "Not working yet!"
 echo ""
-#nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI
+nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/nMinusOne.log
 
 # MC vs data
 echo "controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
 echo "Not working yet !"
 echo ""
-#controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI
+#controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/controlPlots.log
 
 # BG estimates
 echo "wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI"
-wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI
+wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI > $ODIR/wBackground.log
 
 echo "zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI"
-zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI
+zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI > $ODIR/zBackground.log
 
 echo "qcdBackground -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI"
-qcdBackground -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI
+qcdBackground -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI > $ODIR/qcdBackground.log
 
 echo "summary -o $ODIR $LUMI"
-summary -o $ODIR -l $LUMI
+summary -o $ODIR -l $LUMI > $ODIR/summary.log
 
 # set limits
 echo "limits -o $ODIR $LUMI"
-limits -o $ODIR -l $LUMI
+limits -o $ODIR -l $LUMI > $ODIR/limits.log
 
 # tar everything up
 echo "tar -zcvf $ODIR.tgz $ODIR/"
