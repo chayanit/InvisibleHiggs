8d339f88fe5a82d58431e2304a1fae10602aa962
Jim <jim.brooke@cern.ch>
2013-08-08 16:08:28 +0100

    add theory unc to W

--- Analysis/interface/Constants.h
+++ Analysis/interface/Constants.h
@@ -22,17 +22,16 @@ namespace constants {
   static const double syst_effMu  = 0.025;   // 2.5% muon efficiency
 
   static const double syst_lumi    = 0.04;   // 4% lumi
-  static const double syst_Z       = 0.203;  // 3% JES/JER + 20% MCFM/MG ratio
-  static const double syst_WMu     = 0.04;   // 4% JES/JER
-  static const double syst_WEl     = 0.14;   // 14% JES/JER
-  static const double syst_WTau    = 0.27;   // 8% tau ID + 2% W-e cont
+  static const double syst_Z       = 0.21;   // 5% JES/JER + 20% MCFM/MG ratio
+  static const double syst_WMu     = 0.20;   // 4% JES/JER + 20% MCFM/MG ratio
+  static const double syst_WEl     = 0.23;   // 11% JES/JER + 20% MCFM/MG ratio
+  static const double syst_WTau    = 0.27;   // 16% JES/JER + 8% tau ID + 5% W-e cont + 20% MCFM/MG ratio
   static const double syst_QCD1    = 1.5;    // 150% uncertainty from 
-  static const double syst_QCD3    = 0.35;   // 35% from MET shapes in CJV pass/fail
-  static const double syst_TTbar   = 0.40;   // ???
-  static const double syst_Diboson = 0.40;   // ???
-  static const double syst_SingleT = 0.40;   // ???
-  static const double syst_DYLL    = 0.40;   // ???
-
+  static const double syst_QCD3    = 0.83;   // 75% JES/JER + 35% MET shape
+  static const double syst_TTbar   = 0.54;   // 54% JES/JER + 7% x-section
+  static const double syst_Diboson = 0.33;   // 28% JES/JER + 18% x-section
+  static const double syst_SingleT = 0.46;   // 43% JES/JER + 16% x-section
+  static const double syst_DYLL    = 0.83;   // 81% JES/JER + 20% x-section
 
   // systematics - Signal
   static const double syst_Sig_JES[6] = {0.110, 0.121, 0.108, 0.131, 0.077, 0.122};   // really JES + JER + UES
