95e2c792c7b75e1e0606b6e4c9bca83c9f763592
chayanit <chayanit.asawatangtrakuldee@cern.ch>
2014-04-01 19:53:10 +0200

    update run program script

--- Analysis/scripts/runAll.sh
+++ Analysis/scripts/runAll.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+ #!/bin/sh
 
 # script to run the whole analysis
 
-VER=v12
-FILEVER=V12
-LUMI=19576
-RUN=blind     # use either blind or both
+VER=v11f
+FILEVER=V11h
+LUMI=19494
+RUN=both     # use either blind or both
+LEP=central  # turn on lepton ID correction
 
-IDIR=/storage/phjjb/invisibleHiggs/Ntuple$FILEVER/Central
-ODIR=Analysis$FILEVER
+IDIR=/media/disk-1/chayanit/NtupleV11h/Central/
+ODIR=/afs/cern.ch/work/c/chayanit/private/VBFInvHiggs_PATv11NoNu/CMSSW_5_3_7_patch5/src/InvisibleHiggs/Analysis/output/Central/
 
 echo "Deleting and recreating $ODIR"
 if [ -d "$ODIR" ]; then
@@ -17,60 +18,53 @@ fi
 mkdir $ODIR
 
 DATASETS=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_$VER.txt
+DATASETS_scaled=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_$VER_scaled.txt
 DATASETS_W=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_W_$VER.txt
 DATASETS_WTau=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_WTau_$VER.txt
 DATASETS_Z=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_Z_$VER.txt
 DATASETS_QCD=$CMSSW_BASE/src/InvisibleHiggs/Analysis/data/datasets_QCD_$VER.txt
 
 
-# create lepton weight TTree
-echo "writeLeptonWeights -i $IDIR -f $DATASETS"
-#writeLeptonWeights -i $IDIR -f $DATASETS > $ODIR/leptonWeights.log
-
 # MC numbers & plots
-echo "efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
-efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/efficiency.log
+echo "efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI -c $LEP"
+efficiency -i $IDIR -o $ODIR -f $DATASETS -l $LUMI -c $LEP > $ODIR/efficiency.log
 
-echo "nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
-nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/nMinusOne.log
+echo "nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI -c $LEP"
+nMinusOne -i $IDIR -o $ODIR -f $DATASETS -l $LUMI -c $LEP > $ODIR/nMinusOne.log
 echo ""
 
-# MC vs data
-echo "controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
-controlPlots -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/controlPlots.log
+# control plots
+echo "controlPlots -i $IDIR -o $ODIR -f $DATASETS_scaled -l $LUMI"
+controlPlots -i $IDIR -o $ODIR -f $DATASETS_scaled -l $LUMI > $ODIR/controlPlots.log
 echo ""
 
 # BG estimates
-echo "wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI"
-wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI > $ODIR/wBackground.log
+echo "wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI -c $LEP"
+wBackground -i $IDIR -o $ODIR -f $DATASETS_W -l $LUMI -c $LEP > $ODIR/wBackground.log
 echo ""
 
-echo "wTauBackground -i $IDIR -o $ODIR -f $DATASETS_WTau -l $LUMI"
-wTauBackground -i $IDIR -o $ODIR -f $DATASETS_WTau -l $LUMI > $ODIR/wTauBackground.log
+echo "wTauBackground -i $IDIR -o $ODIR -f $DATASETS_WTau -l $LUMI -c $LEP"
+wTauBackground -i $IDIR -o $ODIR -f $DATASETS_WTau -l $LUMI -c $LEP > $ODIR/wTauBackground.log
 echo ""
 
-echo "zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI"
-zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI > $ODIR/zBackground.log
+echo "zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI -c $LEP"
+zBackground -i $IDIR -o $ODIR -f $DATASETS_Z -l $LUMI -c $LEP > $ODIR/zBackground.log
 echo ""
-
-echo "qcdBackground -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI"
-#qcdBackground -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI > $ODIR/qcdBackground.log
+ 
+echo "qcdBackground3 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI -c $LEP"
+qcdBackground3 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI -c $LEP > $ODIR/qcdBackground3.log
 echo ""
-
-echo "qcdBackground2 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI"
-#qcdBackground2 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI > $ODIR/qcdBackground2.log
-echo ""
-
-echo "qcdBackground3 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI"
-qcdBackground3 -i $IDIR -o $ODIR -f $DATASETS_QCD -l $LUMI > $ODIR/qcdBackground3.log
+ 
+echo "search -i $IDIR -o $ODIR -f $DATASETS -l $LUMI"
+search -i $IDIR -o $ODIR -f $DATASETS -l $LUMI > $ODIR/search.log
 echo ""
 
 echo "summary -o $ODIR $LUMI"
 summary -q 3 -o $ODIR -l $LUMI > $ODIR/summary.log
 echo ""
 
-# combine tool limits
-runLimits $ODIR $RUN
+## combine tool limits
+./runLimits.sh $ODIR $RUN
 
 # make plots
 echo "limitPlots -o $ODIR $LUMI"
